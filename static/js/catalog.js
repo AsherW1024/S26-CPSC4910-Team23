ccb6813bb7cafc842f2ba7c8b1c613f30213bd55
brholde <156234663+brholde@users.noreply.github.com>
2026-02-16 17:39:45 -0500

    Added an detailed product information view to the catalog screen when after clicking on a product

--- static/js/catalog.js
+++ static/js/catalog.js
@@ -46,7 +46,7 @@ async function queryProducts() {
 		productDiv.dataset.index = index;
 		//image shown in catalog grid
 		let productImg = document.createElement("img")
-		productImg.src = product.images[0];
+		productImg.src = product.thumbnail;
 		//product name show in the catalog grid
 		let name = document.createElement("h3");
 		name.innerText = product.title;
@@ -58,6 +58,9 @@ async function queryProducts() {
 		productDiv.appendChild(productImg);
 		productDiv.appendChild(name);
 		productDiv.appendChild(price);
+
+		//give product div an event listener that opens up the advanced details
+		productDiv.addEventListener("click", showProductDetails)
 		
 		//append elements to the grid;
 		grid.appendChild(productDiv);
@@ -83,6 +86,116 @@ function toggleMenu() {
 		filterMenu.classList.toggle("closed");
 		filterButton.innerText = "Sorting & Filters ▼";
 	}
+}
+
+function closePopup() {
+	let popupEl = document.getElementById("popup-overlay-bg");
+	popupEl.innerHTML = "";
+	popupEl.hidden = true;
+}
+
+function showProductDetails() {
+	let productDetailIndex = this.dataset.index;
+	let productDetails = pageProductData[productDetailIndex];
+	console.log(productDetails)
+
+	let productDetailsHtml = `
+		<div id="popup-content-bg">
+			<div id="popup-content-div">
+				<button id="popup-close">X</button>
+				<div id="popup-images"></div>
+				<h2>${productDetails.title}</H3>
+				<div id="popup-price">
+					<div class="side-by-side">
+						<h3>Price:</h3>
+						<p>$${productDetails.price}</p>
+					</div>
+				</div>
+				<div id="popup-availability">
+					<div class="side-by-side">
+						<h3>Availability:</h3>
+						<p>${productDetails.availabilityStatus}</p>
+					</div>
+					<div class="side-by-side">
+						<h3>Stock:</h3>
+						<p>${productDetails.stock}</p>
+					</div>
+				</div>
+				<div id="popup-brand">
+					<div class="side-by-side">
+						<h3>Brand:</h3>
+						<p>${productDetails.brand}</p>
+					</div>
+				</div>
+				<div id="popup-description">
+					<div class="side-by-side">
+						<h3>Description:</h3>
+						<p>${productDetails.description}</p>
+					</div>
+				</div>
+				<div id="popup-dimensions">
+					<h3>Dimensions:</h3>
+					<div class="side-by-side">
+						<h4>Depth:</h4>
+						<p>${productDetails.dimensions["depth"]} in</p>
+					</div>
+					<div class="side-by-side">
+						<h4>height:</h4>
+						<p>${productDetails.dimensions["height"]} in</p>
+					</div>
+					<div class="side-by-side">
+						<h4>width:</h4>
+						<p>${productDetails.dimensions["width"]} in</p>
+					</div>
+				</div>
+				<div id="popup-rating">
+					<h3>Overall Rating</h3>
+					<p>${productDetails.rating}/5</p>
+				</div>
+				<div id="popup-reviews">
+					<h3>Reviews</h3>
+				</div>
+			</div>
+		</div>
+	`;
+
+	//add product popup html to page
+	let popupEl = document.getElementById("popup-overlay-bg");
+	popupEl.innerHTML = productDetailsHtml;
+
+	//add each image
+	let imageDiv = document.getElementById("popup-images");
+	productDetails.images.forEach(image => {
+		let imageEl = document.createElement("img");
+		imageEl.src = image;
+		imageDiv.appendChild(imageEl);
+	})
+
+	//add each review
+	productDetails.reviews.forEach(review => {
+		let reviewDiv = document.createElement("div");
+		reviewDiv.classList.add("review-div");
+		let reviewName = document.createElement("h4");
+		reviewName.innerText = review.reviewerName;
+		let reviewScore = document.createElement("p");
+		reviewScore.innerText = `${review.rating}/5`;
+		let reviewComment = document.createElement("p");
+		reviewComment.innerText = review.comment;
+
+		//establish relationships between elements
+		reviewDiv.appendChild(reviewName);
+		reviewDiv.appendChild(reviewScore);
+		reviewDiv.appendChild(reviewComment);
+		
+		//add to reviews div
+		document.getElementById("popup-reviews").appendChild(reviewDiv);
+	});
+
+	//add close button listener
+	document.getElementById("popup-close").addEventListener("click", closePopup);
+
+	//show product page popup
+	popupEl.hidden = false;
 }
 
 //one time pull for products when user loads the page for the first time
