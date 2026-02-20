849d551b95d47fd0717561e5939e6a17bfc68a9b
brholde <156234663+brholde@users.noreply.github.com>
2026-02-19 19:36:06 -0500

    cosmetic tweaks for point value page

--- static/js/point_value.js
+++ static/js/point_value.js
@@ -4,13 +4,13 @@ let dropdownButton = document.getElementById("change-points-dropdown");
 let controlMenu = document.getElementById("point-value-control");
 let errorMessageEl = document.getElementById("error-message");
 let pointValueEl = document.getElementById("current-point-value");
+let inputBox = document.getElementById("point-value-input");
 
 //dropdown button state
 let controlsHidden = true;
 
 //send point request value to backend, validate, get back response
 function pointValueRequest() {
-	let inputBox = document.getElementById("point-value-input");
 	let newPointVal = inputBox.value;
 
 	fetch("/point_value", {
@@ -51,8 +51,32 @@ function toggleMenu() {
 	}
 }
 
+//hide error message text and clear its contents
+function clearErrorMessage() {
+	errorMessageEl.innerText = "";
+	errorMessageEl.hidden = true;
+}
+
+//make point value always display with 2 decimal places
+function formatPointVal() {
+	let val = inputBox.value;
+	val = parseFloat(val).toFixed(2);
+	inputBox.value = val;
+
+	let dbPointVal = pointValueEl.innerText.slice(1);
+	dbPointVal = parseFloat(dbPointVal).toFixed(2);
+	pointValueEl.innerText = `$${dbPointVal}`;
+}
+
 //submit button event listener setup
 submitButton.addEventListener("click", pointValueRequest);
 
 //dropdown buttone event listener setup
 dropdownButton.addEventListener("click", toggleMenu);
+
+//event listeners to clear error messages after input adjustments
+inputBox.addEventListener("input", clearErrorMessage);
+
+//event listeners to always show point values as having 2 decimal places
+inputBox.addEventListener("input", formatPointVal);
+window.addEventListener("load", formatPointVal);
