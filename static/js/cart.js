0a36d8245cd92c25f7e68a454fe5bbc4ca8e84ba
brholde <156234663+brholde@users.noreply.github.com>
2026-03-12 18:36:12 -0400

    popup on cart screen should be complete

--- static/js/cart.js
+++ static/js/cart.js
@@ -17,6 +17,8 @@ for (const button of removeButtons) {
 }
 
 async function showProductPopup(event) {
+	//dont show popup if user was interacting with a button
+	if (event.target.tagName === "BUTTON") {return}
 	productID = event.target.closest(".product-row").dataset.productId;
 	popupDiv = document.getElementById("popup-overlay-bg");
 	response = await fetch(`/product/${productID}`);
@@ -24,6 +26,9 @@ async function showProductPopup(event) {
 		popupHtml = await response.text();
 		popupDiv.innerHTML = popupHtml;
 		popupDiv.hidden = false;
+		const popupScript = document.createElement("script");
+		popupScript.src = "/static/js/product_popup.js";
+		document.body.appendChild(popupScript);
 	}
 }
 
